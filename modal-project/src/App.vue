7e72de6b7b1b6274da2ca7965a1d02aada277b30
thorn573 <thorn573@umn.edu>
2021-07-29 14:28:09 -0500

    Tutorial #4 - Complete
    #4 : Set up Vue CLI, learned CLI basics, and how to use multiple components.

---
+++ modal-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1> {{ title }} </h1>
+  <Modal />
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App :)',
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 3px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
